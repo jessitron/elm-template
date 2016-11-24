517d5c9b95d5ebb35a508efbbe9c5f961f305c0e
jessitron <jessitron@gmail.com>
2016-11-24 08:35:29 -0600

    Make the initial generator create a static page

--- src/Main.elm
+++ src/Main.elm
@@ -3,47 +3,6 @@ module Main exposing (main)
 import Html exposing (Html)
 
 
-main : Program Never Model Msg
+main : Html Never
 main =
-    Html.beginnerProgram
-        { model = model
-        , update = update
-        , view = view
-        }
-
-
-
--- MODEL
-
-
-type alias Model =
-    {}
-
-
-model : Model
-model =
-    {}
-
-
-
--- UPDATE
-
-
-type Msg
-    = Noop
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Noop ->
-            model
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
     Html.div [] []
