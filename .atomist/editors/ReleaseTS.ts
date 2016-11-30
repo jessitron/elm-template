dbc3f07034960eacbd398bdc0c71a8c43678a1ab
jessitron <jessitron@gmail.com>
2016-11-30 06:05:56 -0600

    latest from scala but it doesn't work

--- .atomist/editors/ReleaseTS.ts
+++ .atomist/editors/ReleaseTS.ts
@@ -2,8 +2,7 @@ import {ParameterlessProjectEditor} from "@atomist/rug/operations/ProjectEditor"
 import {Parameters} from "@atomist/rug/operations/Parameters"
 import {Status, Result} from "@atomist/rug/operations/Result"
 import {Project} from '@atomist/rug/model/Core'
-import {Match} from '@atomist/rug/tree/PathExpression'
-import {PathExpression,PathExpressionEngine,TreeNode} from '@atomist/rug/tree/PathExpression'
+import {Match,PathExpression,PathExpressionEngine,TreeNode} from '@atomist/rug/tree/PathExpression'
 
 import {editor, inject} from '@atomist/rug/support/Metadata'
 
@@ -30,7 +29,10 @@ ${description.value()}
        `);
      }
 
-     var readme = project.files();
+     var org = "jessitron";
+
+     var readme = project.findFile("README.md");
+     readme.append("\nSee this application in action: [https://" + org + ".github.io/" + project.name().toLowerCase() + "]()" )
 
      return new Result(Status.Success, "yay woo");
 
